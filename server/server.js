d44958a1e33a345b6ef75db2c41af4d79bb59327
recordboy <ksj_design@naver.com>
2020-09-11 20:40:29 +0900

    크롤링 모듈 및 페펫티어 추가

--- server/server.js
+++ server/server.js
@@ -4,11 +4,31 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 3001;
 
-app.use(cors());
-
 app.use(bodyParser.json());
 app.use("/api", (req, res) => res.json({ username: "bryan" }));
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`express is running on ${port}`);
 });
+
+const puppeteer = require("puppeteer");
+const cheerio = require("cheerio");
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  // await page.goto('https://www.goodchoice.kr/product/search/2');
+  await page.goto("https://place.map.kakao.com/m/main/v/20615157");
+  const content = await page.content();
+  //////////////////////////////////////////////////////////////////
+
+  console.log(content);
+
+  //////////////////////////////////////////////////////////////////
+  await browser.close();
+})();
+
+//////////////////////////////////////////////// //
+app.use("/static", express.static("public"));
+////////////////////////////////////////////////
