a46d2ff79304716e8bce95b3b536ec372f561e48
recordboy <ksj_design@naver.com>
2020-09-11 20:57:25 +0900

    서버 요청 추가

--- server/server.js
+++ server/server.js
@@ -5,7 +5,13 @@ const bodyParser = require("body-parser");
 const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
-app.use("/api", (req, res) => res.json({ username: "bryan" }));
+app.use("/api", (req, res) => {
+  
+  getData();
+  // res.json({ username: "bryan" });
+});
+
+// cors
 app.use(cors());
 
 app.listen(port, () => {
@@ -15,16 +21,17 @@ app.listen(port, () => {
 const puppeteer = require("puppeteer");
 const cheerio = require("cheerio");
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  // await page.goto('https://www.goodchoice.kr/product/search/2');
-  await page.goto("https://place.map.kakao.com/m/main/v/20615157");
-  const content = await page.content();
-  //////////////////////////////////////////////////////////////////
+const getData = (url) => {
+  (async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto("https://place.map.kakao.com/m/main/v/20615157");
+    const content = await page.content();
+    //////////////////////////////////////////////////////////////////
 
-  console.log(content);
+    console.log(content);
 
-  //////////////////////////////////////////////////////////////////
-  await browser.close();
-})();
+    //////////////////////////////////////////////////////////////////
+    await browser.close();
+  })();
+};
