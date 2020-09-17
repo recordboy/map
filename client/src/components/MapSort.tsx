119d0d43cf0d8fe72e79f43da510f8888ccf0ada
recordboy <ksj_design@naver.com>
2020-09-17 18:15:35 +0900

    수정

--- client/src/components/MapSort.tsx
+++ client/src/components/MapSort.tsx
@@ -58,11 +58,17 @@ const MapSort = (props: {
     setAlphabeticalSort(alphabeId);
 
     // 요청 추가
-    fetch("http://localhost:3001/api?userId=1")
+    fetch("http://localhost:3001/api")
       .then((res) => res.json())
       .then((data) => {
         console.log(data)
       });
+    // // 요청 추가
+    // fetch("http://localhost:3001/api?userId=1")
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     console.log(data)
+    //   });
   }
 
   return (
