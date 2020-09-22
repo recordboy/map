6e1a8db5c51dee29868a8af7c7aae0af5cc02232
recordboy <ksj_design@naver.com>
2020-09-22 21:02:28 +0900

    정보 가져오기 수정

--- client/src/components/MapItem.tsx
+++ client/src/components/MapItem.tsx
@@ -9,8 +9,19 @@ const MapItem = (props: {
 
   const { id, place, selectMarker } = props;
 
-  // 장소 정보
-  const [placeInfo, setPlaceInfo] = useState({});
+  // 장소 정보 초기화
+  const [placeInfo, setPlaceInfo] = useState({
+    basicInfo: {
+      placenamefull: ''
+    },
+    // menuInfo: {
+    //   bottomList: [
+    //       {
+    //         price: ''
+    //     }
+    //   ]
+    // }
+  });
 
   // 관련 태그
   let tagArr: string[] = [];
@@ -30,9 +41,8 @@ const MapItem = (props: {
       .then((data) => {
         setPlaceInfo(data);
 
-        console.log(placeInfo)
-
-        // console.log(placeInfo);
+        // 이곳에 있는 정보로 데이터 세팅
+        console.log(data)
       });
   }
   
@@ -41,21 +51,25 @@ const MapItem = (props: {
       onClick={() => {
         selectMarker(id);
         getPlaceInfo(place);
-
-        console.log(placeInfo);
       }}
     >
-      <div className="tag">
-        <span>{tagArr[0]}</span>
-        <span>{tagArr[1]}</span>
-        {tagArr[2] !== undefined && <span>{tagArr[2]}</span>}
+      <div>
+        <div className="tag">
+          <span>{tagArr[0]}</span>
+          <span>{tagArr[1]}</span>
+          {tagArr[2] !== undefined && <span>{tagArr[2]}</span>}
+        </div>
+        <div className="name">{place.place_name}</div>
+        <div className="address">{place.road_address_name}</div>
+        <a href={'tel:' + phone} className="phone">{phone}</a>
+        <a href={place.place_url} className="url" target="_balnk">More</a>
       </div>
-      <div className="name">{place.place_name}</div>
-      <div className="address">{place.road_address_name}</div>
-      <a href={'tel:' + phone} className="phone">{phone}</a>
-      <a href={place.place_url} className="url" target="_balnk">More</a>
-    {/* <div>{placeInfo.basicInfo.placenamefull}</div> */}
-    {/* <div>{console.log(1)}</div> */}
+      <div>
+        <div>{placeInfo.basicInfo.placenamefull}</div>
+        {/* <div>{placeInfo.menuInfo.bottomList[0].price}</div> */}
+      </div>
+
+
     </div>
   );
 };
