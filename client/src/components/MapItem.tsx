c1210a5075eb20d5555ee6998b011a8897c3a608
recordboy <ksj_design@naver.com>
2020-09-23 20:19:22 +0900

    장소 데이터 가져오기 추가

--- client/src/components/MapItem.tsx
+++ client/src/components/MapItem.tsx
@@ -1,51 +1,95 @@
 import React, { useState } from "react";
+import MapPhotoItem from "./MapPhotoItem";
 
 const MapItem = (props: {
   id: number;
   place: any;
   selectMarker: (id: number) => void;
-
 }) => {
-
   const { id, place, selectMarker } = props;
 
   // 장소 정보 초기화
-  const [placeInfo, setPlaceInfo] = useState({
-    basicInfo: {
-      placenamefull: ''
-    },
-    // menuInfo: {
-    //   bottomList: [
-    //       {
-    //         price: ''
-    //     }
-    //   ]
-    // }
+  const [placeData, setPlaceData] = useState({
+    introduction: "",
+    holidayName: "",
+    score: 0,
+    menuInfo: [
+      {
+        menu: "",
+        price: "",
+      },
+    ],
+    photoList: [
+      {
+        orgurl: "",
+      },
+    ],
   });
+
+  // 장소 정보 보여짐 유무
+  const [isOn, setIsOn] = useState(false);
+  
+  const list: any = [];
 
   // 관련 태그
   let tagArr: string[] = [];
   let tag: string = place.category_name;
   tag = tag.replace(/(\s*)/g, "");
-  tagArr = tag.split('>');
+  tagArr = tag.split(">");
 
   // 전화번호
-  let phone: string = '';
-  place.phone !== '' && (phone += place.phone);
+  let phone: string = "";
+  place.phone !== "" && (phone += place.phone);
 
   // 로컬 서버에 장소 정보 요청
   const getPlaceInfo = (place: any) => {
-
     fetch(`http://localhost:3001/api?id=${place.id}`)
       .then((res) => res.json())
-      .then((data) => {
-        setPlaceInfo(data);
-
+      .then((dataJSON) => {
+        
         // 이곳에 있는 정보로 데이터 세팅
-        console.log(data)
+        // console.log(dataJSON);
+
+        // 장소 정보 세팅
+        setPlaceInfo(dataJSON);
       });
-  }
-  
+  };
+
+  // 장소 정보 세팅
+  const setPlaceInfo = (dataJSON: any) => {
+
+    // 소개
+    const introduction: string = dataJSON.basicInfo.introduction;
+
+    // 휴무일
+    const holidayName: string = dataJSON.basicInfo.openHour.offdayList[0].holidayName;
+
+    // 평점
+    const scoreCnt: number = dataJSON.comment.scorecnt;
+    const scoreSum: number = dataJSON.comment.scoresum;
+    const score: number = Math.floor((scoreSum / scoreCnt) * 10) / 10;
+
+    // 메뉴
+    const menuInfo: any[] = dataJSON.menuInfo.bottomList;
+
+    // 사진
+    const photoList: any[] = dataJSON.photo.photoList;
+
+    setPlaceData({
+      introduction: introduction,
+      holidayName: holidayName,
+      score: score,
+      menuInfo: [...menuInfo],
+      photoList: [...photoList],
+    });
+
+    photoList.forEach((item: any, idx: number) => {
+      list.push(<MapPhotoItem key={idx} />);
+    })
+
+    setIsOn(true);
+  };
+
   return (
     <div
       onClick={() => {
@@ -53,7 +97,7 @@ const MapItem = (props: {
         getPlaceInfo(place);
       }}
     >
-      <div>
+      <div className="basic">
         <div className="tag">
           <span>{tagArr[0]}</span>
           <span>{tagArr[1]}</span>
@@ -61,15 +105,27 @@ const MapItem = (props: {
         </div>
         <div className="name">{place.place_name}</div>
         <div className="address">{place.road_address_name}</div>
-        <a href={'tel:' + phone} className="phone">{phone}</a>
-        <a href={place.place_url} className="url" target="_balnk">More</a>
+        <a href={"tel:" + phone} className="phone">
+          {phone}
+        </a>
+        <a href={place.place_url} className="url" target="_balnk">
+          More
+        </a>
       </div>
-      <div>
-        <div>{placeInfo.basicInfo.placenamefull}</div>
-        {/* <div>{placeInfo.menuInfo.bottomList[0].price}</div> */}
+      <div className={isOn ? "detail on" : "detail"}>
+
+        <div className="photo">
+          <div className="inner">
+            {list}
+            {/* <img src={placeData.photoList[1].orgurl} /> */}
+          </div>
+        </div>
+        <div>{placeData.introduction}</div>
+        <div>{placeData.score}</div>
+        <div>{placeData.holidayName}</div>
+        <div>{placeData.menuInfo[0].menu}</div>
+        <div>{placeData.menuInfo[0].price}</div>
       </div>
-
-
     </div>
   );
 };
---
+++ client/src/components/MapPhotoItem.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+const MapPhotoItem = (props: {
+  // id: number;
+  // url: any;
+}) => {
+  // const { id, url } = props;
+
+
+  return (
+    <div></div>
+  );
+};
+
+export default MapPhotoItem;
