557f02bb6a26d667757809e2ed119093545282f7
Record Man <ksj_design@naver.com>
2020-10-05 11:53:46 +0900

    [기능] 검색 버튼 json 파일로 리스트 생성

--- client/src/components/MapSearch.tsx
+++ client/src/components/MapSearch.tsx
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
 
+// 검색 항목 가져오기
+import searchList from "../search-list.json";
+
 const MapSearch = (props: {
   handleKeywordSearch: (item: string) => void;
   handleInputSearch: (item: string) => void;
 }) => {
   const { handleKeywordSearch, handleInputSearch } = props;
   const [inputData, setInputData] = useState("");
+  const [morePlace, setMorePlace] = useState({
+    depth: 1,
+    selectDepth: "",
+    selectList: [],
+  });
 
   // 검색 항목 더보기 좌표
   let startY: number = 0;
@@ -16,79 +24,74 @@ const MapSearch = (props: {
   };
 
   const moreY = (e: any) => {
-    if (e.type === 'touchstart') {
+    if (e.type === "touchstart") {
       startY = e.changedTouches[0].clientY;
     }
-    if (e.type === 'touchend') {
+    if (e.type === "touchend") {
       endY = e.changedTouches[0].clientY;
-    }
-    if (endY > startY && endY - startY > 30) {
-      viewMorePlace();
+      if (endY > startY && endY - startY > 30) {
+        viewMorePlace(e.target.innerText);
+      }
     }
   };
 
-  const viewMorePlace = () => {
-    console.log(1);
+  const viewMorePlace = (name: string) => {
+    let list: any;
+    searchList.forEach((item: any) => {
+      if (item.name === name) {
+        list = item.detail;
+      }
+    });
+
+    setMorePlace({
+      depth: 2,
+      selectDepth: name,
+      selectList: list,
+    });
   };
 
   return (
     <div className="controller">
       <div className="sort">
-        <div className="inner">
-          <button
-            type="button"
-            onClick={(e) => {
-              clickKeywordBtn(e);
-            }}
-            onTouchStart={(e) => {
-              moreY(e);
-            }}
-            onTouchEnd={(e) => {
-              moreY(e);
-            }}
-          >
-            음식
-          </button>
-          <button
-            type="button"
-            onClick={(e) => {
-              clickKeywordBtn(e);
-            }}
-          >
-            편의점
-          </button>
-          <button
-            type="button"
-            onClick={(e) => {
-              clickKeywordBtn(e);
-            }}
-          >
-            약국
-          </button>
-          <button
-            type="button"
-            onClick={(e) => {
-              clickKeywordBtn(e);
-            }}
-          >
-            마트
-          </button>
-          <button
-            type="button"
-            onClick={(e) => {
-              clickKeywordBtn(e);
-            }}
-          >
-            마트
-          </button>
-          <button
-            type="button"
-            onClick={(e) => {
-              clickKeywordBtn(e);
-            }}
-          >
-            화장실
-          </button>
+        <div className={morePlace.depth !== 1 ? "depth01 disabled" : "depth01"}>
+          <div className="inner" style={{ width: searchList.length * 80 + "px" }}>
+            {searchList.map((item: any, idx: number) => {
+              return (
+                <button
+                  key={idx}
+                  type="button"
+                  onClick={(e) => {
+                    clickKeywordBtn(e);
+                  }}
+                  onTouchStart={(e) => {
+                    moreY(e);
+                  }}
+                  onTouchEnd={(e) => {
+                    moreY(e);
+                  }}
+                >
+                  {item.name}
+                </button>
+              );
+            })}
+          </div>
+        </div>
+        <div className={morePlace.depth === 2 ? "depth02 on" : "depth02"}>
+          <div className="inner" style={{ width: morePlace.selectList.length * 80 + "px" }}>
+            {morePlace.selectList.map((item: any, idx: number) => {
+              return (
+                <button
+                  key={idx}
+                  type="button"
+                  onClick={(e) => {
+                    clickKeywordBtn(e);
+                  }}
+                >
+                  {item}
+                </button>
+              );
+            })}
+          </div>
         </div>
       </div>
 
