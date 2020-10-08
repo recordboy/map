60b4f5162fdc4d51558888f3bcf47e50f8372e36
recordboy <ksj_design@naver.com>
2020-10-08 10:08:14 +0900

    구조 변경

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 3001; // 로컬 전용
-// const port = process.env.PORT || 5000; // heroku 전용
 const fetch = require("node-fetch");
 
 app.use(cors());
