2e4ae0982025f2236b25fd6b599e1d1b9d948186
recordboy <ksj_design@naver.com>
2020-10-08 11:26:12 +0900

    수정

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.listen(port, () => {
   console.log(`express is running on ${port}`);
 });
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
