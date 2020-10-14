353807f34a65d5baa67777641b3439c964cc83a7
Record Man <ksj_design@naver.com>
2020-10-14 17:57:38 +0900

    Update MyPlaceManual.tsx

--- src/components/MyPlaceManual.tsx
+++ src/components/MyPlaceManual.tsx
@@ -9,11 +9,12 @@ const MyPlaceManual = () => {
 
   // 다시 보지 않기 확인
   useEffect(() => {
-    if (myPlaceData) {
-      if (myPlaceData.hasOwnProperty("isOnManualAgain")) {
-        if (myPlaceData.isOnManualAgain === "Y") {
-          setIsOnManual(false);
-        }
+    if (myPlaceData !== null) {
+      if (
+        myPlaceData.hasOwnProperty("isOnManualAgain") &&
+        myPlaceData.isOnManualAgain === "Y"
+      ) {
+        setIsOnManual(false);
       }
     }
   }, []);
@@ -38,11 +39,18 @@ const MyPlaceManual = () => {
       <div
         className="info"
         style={!isOnManual ? { display: "none" } : { display: "block" }}
-      >
+        >
+          <div className="test"></div>
         <div className="balloon type01">
-          위아래로 드래그하여
+          위, 아래로 지도 크기를 변경할 수 있어요!
+        </div>
+        <div className="balloon type02">
+          원하는 항목을 클릭하면 장소가 나와요!
           <br />
-          지도 크기를 변경할 수 있어요!
+          아래로 밀면 자세한 항목이 나와요!
+        </div>
+        <div className="balloon type03">
+          장소를 클릭하면 자세한 정보를 얻을 수 있어요!
         </div>
         <label className="check-again" htmlFor="check01-01">
           <input
@@ -55,9 +63,10 @@ const MyPlaceManual = () => {
                 setIsOnManualAgain(true);
               }
             }}
-          />{" "}
+          />
           다시 보지 않기
         </label>
+
         <button
           className="btn-close"
           onClick={() => {
@@ -73,7 +82,7 @@ const MyPlaceManual = () => {
           setIsOnManual(true);
         }}
       >
-        <i className="fa fa-question" aria-hidden="true"></i>
+        {/* <i className="fa fa-question" aria-hidden="true"></i> */}
       </div>
     </div>
   );
