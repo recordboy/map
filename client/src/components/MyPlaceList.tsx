b604b1ea880c9348d41a7221afab228870876cc6
Record Man <ksj_design@naver.com>
2020-10-15 17:18:43 +0900

    Update MyPlaceList.tsx

--- client/src/components/MyPlaceList.tsx
+++ client/src/components/MyPlaceList.tsx
@@ -4,10 +4,11 @@ import MyPlaceItem from "./MyPlaceItem";
 const MyPlaceList = (props: {
   itemList: any;
   selectMarker: (id: number) => void;
+  setLocalData: (place: any) => void;
 }) => {
-  const { itemList, selectMarker } = props;
+  const { itemList, selectMarker, setLocalData } = props;
   const list = itemList.map((item: any) => {
-    return <MyPlaceItem key={item.id} id={item.id} place={item.place} selectMarker={selectMarker} />;
+    return <MyPlaceItem key={item.id} id={item.id} place={item.place} selectMarker={selectMarker} setLocalData={setLocalData} />;
   });
   return <div className="list">{list}</div>;
 };
