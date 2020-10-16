864e46717db8a01a8af339824fc719a559a8dceb
recordboy <ksj_design@naver.com>
2020-10-16 11:31:30 +0900

    프록시 설정

--- index.js
+++ index.js
@@ -1,45 +1,46 @@
 const express = require('express');
 const app = express()
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
-
 // 서버 확인용
 app.get("/api/greeting", (req,res) => {
   res.send("Hello World!");
 })
 
-// 크로스 설정
-const cors = require("cors");
-app.use(cors());
+module.exports = function(app) {
+  app.use(proxy("/api/greeting", { target: "http://localhost:5000" }));
+};
 
-// 포트 설정
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`express is running on ${PORT}`);
-});
+// // 크로스 설정
+// const cors = require("cors");
+// app.use(cors());
 
-// 테스트용 더미 가져오기
-// const fs = require("fs");
-// const dataBuffer = fs.readFileSync('./data.json');
-// const dataJSON = dataBuffer.toString();
+// // 포트 설정
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`express is running on ${PORT}`);
+// });
 
-// 데이터 가져오기
-const fetch = require("node-fetch");
-app.use("/api/greeting", (req, res) => {
+// // 테스트용 더미 가져오기
+// // const fs = require("fs");
+// // const dataBuffer = fs.readFileSync('./data.json');
+// // const dataJSON = dataBuffer.toString();
 
-  // 테스트용, 더미 가져오기
-  // res.json(dataJSON);
+// // 데이터 가져오기
+// const fetch = require("node-fetch");
+// app.use("/api/greeting", (req, res) => {
 
-  // 장소 id
-  const id = req.param('id');
+//   // 테스트용, 더미 가져오기
+//   // res.json(dataJSON);
 
-  fetch(`https://place.map.kakao.com/m/main/v/${id}`)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (myJson) {
-      const data = JSON.parse(JSON.stringify(myJson));
-      res.json(data);
-    });
-});
+//   // 장소 id
+//   const id = req.param('id');
+
+//   fetch(`https://place.map.kakao.com/m/main/v/${id}`)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (myJson) {
+//       const data = JSON.parse(JSON.stringify(myJson));
+//       res.json(data);
+//     });
+// });
