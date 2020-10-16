26c6bb95e313024d16573c7dde48b2dae8e6cd23
recordboy <ksj_design@naver.com>
2020-10-16 16:31:54 +0900

    init

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
 }
 
-app.get("/api/greeting", (req, res) => {
-  res.send("Hello World!");
-});
-
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
