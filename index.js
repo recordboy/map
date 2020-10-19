d4f12bbae67d5b0451f990f0f592f292bd42ff35
recordboy <ksj_design@naver.com>
2020-10-19 15:37:27 +0900

    init

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express()
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
 // 크로스 설정
 const cors = require("cors");
 app.use(cors());
