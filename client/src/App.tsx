96f77602e260ff908d744b556c1c9528c3a68eb5
recordboy <ksj_design@naver.com>
2020-10-19 20:03:07 +0900

    init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const getData = () => {
+  fetch(`/api/data`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button type="button" onClick={() => {
+        getData();
+      }}>get data</button>
     </div>
   );
 }
