e9503035fdd12d6e8e723b443de1a7b389c7acf6
recordboy <ksj_design@naver.com>
2020-10-19 16:44:39 +0900

    init

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
 const express = require('express');
 const app = express()
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "client/build")));
+// }
+
+app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
-// 크로스 설정
-const cors = require("cors");
-app.use(cors());
+// // 크로스 설정
+// const cors = require("cors");
+// app.use(cors());
 
-// 포트 설정
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`express is running on ${PORT}`);
-});
+// // 포트 설정
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`express is running on ${PORT}`);
+// });
 
-const fetch = require("node-fetch");
-app.use("/api/greeting", (req, res) => {
-  fetch(`https://place.map.kakao.com/m/main/v/27103887`)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (myJson) {
-      const data = JSON.parse(JSON.stringify(myJson));
-      res.json(data);
-    });
-});
+// const fetch = require("node-fetch");
+// app.use("/api/greeting", (req, res) => {
+//   fetch(`https://place.map.kakao.com/m/main/v/27103887`)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (myJson) {
+//       const data = JSON.parse(JSON.stringify(myJson));
+//       res.json(data);
+//     });
+// });
 
 // 테스트용 더미 가져오기
 // const fs = require("fs");
