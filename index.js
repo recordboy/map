c77ac46ee746e1eb8b03305971166a6e4ee1fc99
recordboy <ksj_design@naver.com>
2020-10-19 16:47:50 +0900

    init

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express()
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "client/build")));
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.get("/api/greeting", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build", "index.html"));
