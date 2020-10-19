e962f44b93a98dc285a25870e6f07b3e187cd364
recordboy <ksj_design@naver.com>
2020-10-19 17:39:05 +0900

    init

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const generatePassword = require('password-generator');
+const fetch = require("node-fetch");
 
 const app = express();
 
@@ -8,18 +8,29 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Put all API endpoints under '/api'
-app.get('/api/passwords', (req, res) => {
-  const count = 5;
+app.use('/api/passwords', (req, res) => {
 
-  // Generate some passwords
-  const passwords = Array.from(Array(count).keys()).map(i =>
-    generatePassword(12, false)
-  )
 
-  // Return them as json
-  res.json(passwords);
+  fetch(`https://place.map.kakao.com/m/main/v/27103887`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (myJson) {
+      const data = JSON.parse(JSON.stringify(myJson));
+      res.json(data);
+    });
 
-  console.log(`Sent ${count} passwords`);
+  // const count = 5;
+
+  // // Generate some passwords
+  // const passwords = Array.from(Array(count).keys()).map(i =>
+  //   generatePassword(12, false)
+  // )
+
+  // // Return them as json
+  // res.json(passwords);
+
+  // console.log(`Sent ${count} passwords`);
 });
 
 // The "catchall" handler: for any request that doesn't
