0030d0f40d9490c18486de22363ed60e6cb71e1a
Record Man <ksj_design@naver.com>
2020-10-22 14:43:56 +0900

    Update MyPlaceList.tsx

--- client/src/components/MyPlaceList.tsx
+++ client/src/components/MyPlaceList.tsx
@@ -2,15 +2,19 @@ import React from "react";
 import MyPlaceItem from "./MyPlaceItem";
 
 const MyPlaceList = (props: {
+  result: string;
   itemList: any;
   selectMarker: (id: number) => void;
   setSavePlace: (place: any) => void;
 }) => {
-  const { itemList, selectMarker, setSavePlace } = props;
+  const { result, itemList, selectMarker, setSavePlace } = props;
   const list = itemList.map((item: any) => {
     return <MyPlaceItem key={item.id} id={item.id} place={item.place} selectMarker={selectMarker} setSavePlace={setSavePlace} />;
   });
-  return <div className="place-list">{list}</div>;
+  return <div className="place-list">
+    <div className="main-title">{result}</div>
+    {list}
+  </div>;
 };
 
 export default MyPlaceList;
